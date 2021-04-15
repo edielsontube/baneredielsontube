223e26d41c04b7919d96a9adfcedebf916255d87
edielsontube <80482582+edielsontube@users.noreply.github.com>
2021-04-15 16:34:11 -0300

    Update baner.sh

--- baner.sh
+++ baner.sh
@@ -6,6 +6,6 @@ cd ~/baneredielsontube
 pkg install figlet -y
 apt-get install ruby -y
 gem install lolcat 
-mv bash.bashrc /data/data/com.termux/files/usr/etc
+cp bash.bashrc /data/data/com.termux/files/usr/etc
 figlet TEMINADO
 termux-open https://youtube.com/c/edielsontube
